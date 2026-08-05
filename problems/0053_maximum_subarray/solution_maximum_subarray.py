6995551f822c5e8a3014ede2442b529ab3130359
Aditya Gautam <ag9505545@gmail.com>
2026-08-05 23:09:15 +0530

    Add Maximum Subarray

---
+++ problems/0053_maximum_subarray/solution_maximum_subarray.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = 0
+        max_sum = float("-inf")
+
+        for num in nums:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
